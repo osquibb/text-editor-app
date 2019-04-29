5ccfc678857d87b8e090349648c20d255c95124f
Oliver Squibb <osquibb@gmail.com>
2019-04-29 14:16:54 -0400

    new components

---
+++ src/components/FormatButtonsRowComponent.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react';
+
+export default class FormatButtonsRow extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return(
+      <Fragment>
+        
+
+      </Fragment>
+    );
+  }
+
+}
---
+++ src/components/SaveButtonComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function SaveButton() {
+  return (
+    <h1></h1>
+  );
+}
+
+export default SaveButton;
+
