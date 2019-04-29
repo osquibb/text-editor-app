e629658bc05d62d3e579ea251599f36800c71a5e
Oliver Squibb <osquibb@gmail.com>
2019-04-29 14:10:43 -0400

    initial commit

---
+++ src/components/TextEditorComponent.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+export default class TextEditor extends Component {
+
+  render() {
+    return(
+      <Fragment>
+        
+
+      </Fragment>
+    );
+  }
+
+}
