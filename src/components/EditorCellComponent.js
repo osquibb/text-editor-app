759d461418176a8b2f935ea528b11ceb095bbba6
Oliver Squibb <osquibb@gmail.com>
2019-04-29 14:42:30 -0400

    commit

--- src/components/EditorCellComponent.js
+++ src/components/EditorCellComponent.js
@@ -1,21 +1,29 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+
 
 export default class EditorCell extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-
+      formattedText: ''
     };
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(event) {
+    this.setState({formattedText: event.target.value});
   }
 
   render() {
     return(
-      <Fragment>
-        
-
-      </Fragment>
+      <input type="textarea" 
+             value={this.state.formattedText}
+             onChange={this.handleTextChange}
+      />
     );
+
   }
 
 }
--- src/components/TextEditorComponent.js
+++ src/components/TextEditorComponent.js
@@ -1,13 +1,11 @@
 import React, { Component, Fragment } from 'react';
+import EditorCell from './EditorCellComponent';
 
 export default class TextEditor extends Component {
 
   render() {
     return(
-      <Fragment>
-        
-
-      </Fragment>
+      <EditorCell />
     );
   }
 
