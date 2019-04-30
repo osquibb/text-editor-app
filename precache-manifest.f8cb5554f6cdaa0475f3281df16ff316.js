bd8e3f940e1a4a62878066f78029c9b8586b95a2
Oliver Squibb <osquibb@gmail.com>
2019-04-30 18:37:26 -0400

    Updates

---
+++ precache-manifest.f8cb5554f6cdaa0475f3281df16ff316.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6304f69cd6150675b14c4d4d69528bcd",
+    "url": "/text-editor-app/index.html"
+  },
+  {
+    "revision": "74ed8537c6d3cf7f3a72",
+    "url": "/text-editor-app/static/css/2.7daadf64.chunk.css"
+  },
+  {
+    "revision": "549bbc9e184caaa5607c",
+    "url": "/text-editor-app/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "74ed8537c6d3cf7f3a72",
+    "url": "/text-editor-app/static/js/2.90535e87.chunk.js"
+  },
+  {
+    "revision": "549bbc9e184caaa5607c",
+    "url": "/text-editor-app/static/js/main.6439e395.chunk.js"
+  },
+  {
+    "revision": "e71841a60b89d4111e78",
+    "url": "/text-editor-app/static/js/runtime~main.35563332.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/text-editor-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/text-editor-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/text-editor-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/text-editor-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/text-editor-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
